7355c9f0230ae2c0ec146e399f8c8c0f19a53ed4
Trey Annis <ta08annis@gmail.com>
2021-06-16 14:57:11 -0400

    home page format and template created, adding on to it. Fonts loading properly, and feel for what I want the website to be is all there.

--- src/home.js
+++ src/home.js
@@ -3,22 +3,66 @@ import mo from './big-mo.jpeg';
 
 const home = () => {
 
-    const element = document.createElement('div');
- 
-    element.classList.add('hello');
-    element.textContent = "HELLO";
+    const restaurantHeader = () => {
+        const element = document.createElement('div');
+        const headerOne = document.createElement('h1')
+        element.classList.add('restaurant-header');
+        headerOne.textContent = "Big Mo's Brunch Bazaar";
+        element.appendChild(headerOne);
+
+        return element;
+    }
+
 
 
     // Add the image to our existing div.
     
+    const img = () => {
+        const img = document.createElement('div');
+        img.classList.add('big-mo-image')
+        const bigMo = new Image();
+        bigMo.src = mo;
+        img.appendChild(bigMo);
+        console.log(typeof element);
+        
+        return img;
+    }
 
-    const img = document.createElement('div');
-    const bigMo = new Image();
-    bigMo.src = mo;
-    img.appendChild(bigMo);
-    console.log(typeof element);
     
-    return [element, img]
+    const about = () => {
+        const about = document.createElement('div');
+        const headerTwo = document.createElement('h2');
+        const paragraphOne = document.createElement('p');
+        about.classList.add('about-header');
+        headerTwo.textContent = "Who We Are";
+        paragraphOne.innerHTML = "Mo started <strong>Big Mo's Brunch Bazaar</strong> in pursuit of his dream of serving felines the best quality brunch in Catville, while at the same time providing world-class customer service. Mo prides himself in giving only an exceptional customer experience to each and every cat that enters the bazaar. Come on and drop by!"
+        about.appendChild(headerTwo);
+        about.appendChild(paragraphOne);
+
+        return about;
+    }
+
+    const location = () => {
+        const location = document.createElement('div');
+        const headerTwo = document.createElement('h2');
+        const paragraphOne = document.createElement('p');
+        const paragraphTwo = document.createElement('p');
+        location.classList.add('location-header');
+        headerTwo.textContent = "Location";
+        paragraphOne.textContent = "111 Fur St.";
+        paragraphTwo.textContent = "Catville, CA 22222"
+    
+        location.appendChild(headerTwo);
+        location.appendChild(paragraphOne);
+        location.appendChild(paragraphTwo);
+
+        return location;
+    }
+
+
+
+
+    return [restaurantHeader(), img(), about(), location()]
 };
 
 export { home }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './style.css';
 import mo from './big-mo.jpeg';
 import { home } from './home';
+import { menu } from './menu';
+import { contact } from './contact';
 
 // home module
 const homePage = (() => {
+    const content = document.getElementById('content')
     for (const [index, element] of home().entries()) {
-        document.body.appendChild(home()[index]);
+        content.appendChild(home()[index]);
     } 
     
 
